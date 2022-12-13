11b5b461a3d22cc2d6d8d84174bf0f5b9b2a3321
Joel Azwar <joel_azwar@yahoo.com>
2022-12-13 16:10:24 -0500

    add more gifs, and carousell/gallery

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-
+//randomize background
 var num = Math.ceil( Math.random() * 5 );
 document.body.style.backgroundImage =`url(images/bg/canvas${num}.gif)`;
+
+//carousel script
+const track=document.querySelector('.track')
+const slides = Array.from(track.children)
+const nextBtn = document.querySelector('.btn-right')
+const prevBtn = document.querySelector('.btn-left')
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+let curSlide = 0;
+
+let maxSlide = slides.length - 1;
+
+slides.forEach((slide,indx)=>{
+  slide.style.transform = `translateX(${indx * 100}%)`;
+})
+
+nextBtn.addEventListener("click", e=>{
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+prevBtn.addEventListener("click", e=>{
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
